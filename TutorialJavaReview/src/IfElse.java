27f3944fa3f36267844d339219fb6ca184e86920
eriDam <erika.c.caballero@gmail.com>
2014-08-13 21:16:08 +0200

    First change in second tutorial
    Add var in condition and system.out.println

--- TutorialJavaReview/src/IfElse.java
+++ TutorialJavaReview/src/IfElse.java
@@ -11,10 +11,16 @@ public class IfElse {
 		if (puntuacion >= 5) {
 			System.out.println("Enhorabuena has aprobado!!");
 			System.out.println("Tu nota final es: "+puntuacion);
+			//Si la condicion se cumple lo introducimos en el grupo A
+			grupo='A';
 		}else{
 			System.out.println("Lo siento, pero has suspendido...");
+			//Si la condicion no se cumple lo introducimos en el grupo B
+			grupo='B';	
 		}
-
+		//Después el programa continua las lineas siguientes y nos muestra según la calificación
+		//obtenida, el grupo al que pertenece el usuario
+		System.out.println("*** Según la calificación obtenida que ha sido: "+puntuacion+" ***\n***      Ahora formas parte del grupo: "+grupo+" ***");
 	}
 
 }
