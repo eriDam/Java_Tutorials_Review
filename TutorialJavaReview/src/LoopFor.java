f2821639ea57890c22adc4470622dc372c6e1ec5
eriDam <erika.c.caballero@gmail.com>
2014-08-14 21:37:46 +0200

    Eighth tutorial, counter with loop for

---
+++ TutorialJavaReview/src/LoopFor.java
@@ -0,0 +1,11 @@
+import static java. lang. System. out;
+public class LoopFor {
+
+	public static void main(String[] args) {
+		// Contadores con loop for
+	for (int contador = 1;contador <= 10; contador++){
+		out.println("El valor del contador es: "+contador+".");
+	}
+	out.print("¡Hecho!");
+}
+}
