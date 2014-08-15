857051a8cb432b581a3bca60d0106f0f671bfc17
eriDam <erika.c.caballero@gmail.com>
2014-08-15 19:04:34 +0200

    Fifteenth. Tutorial example Bidimensional array

---
+++ TutorialJavaReview/src/ArrayBidimensional.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayBidimensional {
+
+	public static void main(String[] args) {
+		// Se crean con 2 corchetes y el nombre del array
+		boolean [][] puntoSeleccionado = new boolean [50][50];
+		puntoSeleccionado [4][13] = true;
+		puntoSeleccionado [7][6] = true;
+		puntoSeleccionado [11][22] = true;
+		//Cada dimension tiene 50. Puede llegar a contener 2500
+		
+			
+	}
+
+}
